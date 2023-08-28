5b2629ed2a049741a7683e44487f9c13e2a3878f
houikhalfan <h.ouikhalfan@gmail.com>
2023-08-28 16:03:24 -0700

    taU

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
                 print(i, end='')
                 c += 1
         print()
-        return (c)
-    except:
-        return c
+    except IndexError:
+        pass
+    return (c)
