a9fbe818d15d04f8571f6ec866291c8dc064ea94
houikhalfan <h.ouikhalfan@gmail.com>
2023-08-29 16:46:23 -0700

    ko

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,6 @@ not an empty one
 class Square:
     """ square class """
     def __init__(self, size):
-        """ size ini"""
-        self._size = size
+        """ size ini
+        """
+        self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,10 @@ square class
 class Square:
     """ square claasss"""
     def __init__(self, size=0):
-        """ int size"""
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self._size = size
+        """ int size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
