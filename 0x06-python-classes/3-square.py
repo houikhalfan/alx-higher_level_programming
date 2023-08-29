96e93a668735cfbffe91cf1fcc4a8f4190b947ce
houikhalfan <h.ouikhalfan@gmail.com>
2023-08-29 16:54:42 -0700

    t3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" square class again
+"""
+
+
+class Square:
+    """ square class ag"""
+    def __init__(self, size=0):
+        """ attrib inistialisation
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
