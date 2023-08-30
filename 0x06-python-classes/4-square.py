9a27d97e76e56b7f62387d16ee065e224b8114cd
houikhalfan <h.ouikhalfan@gmail.com>
2023-08-29 17:02:30 -0700

    tt4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+square class
+"""
+
+
+class Square:
+    """ square claasss"""
+    def __init__(self, size=0):
+        """ int size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """retr size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ exceptions for size"""
+        if type(value) is int and value >= 0:
+            self.__size = value
+        elif type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
