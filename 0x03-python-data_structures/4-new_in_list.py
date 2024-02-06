a05727a36d6ceebbeff7d3f2494a08c044ec6255
houikhalfan <h.ouikhalfan@gmail.com>
2024-02-06 03:03:51 -0800

    ee

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-new_list = my_list.copy()
-for i in range(len(new_list)):
-if i == idx:
-new_list[i] = element
-return new_list
-return my_list
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if i == idx:
+            new_list[i] = element
+            return new_list
+    return my_list
