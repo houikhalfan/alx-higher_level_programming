0e094c3306c4cef380de795e1eb69c5d8748840a
houikhalfan <h.ouikhalfan@gmail.com>
2024-03-06 06:00:58 -0800

    ta

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+def a trg class
+"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """defining two parameter"""
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for the private instance attribute height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area calc"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """peri calc"""
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        else:
+            return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        """fach command str() ayakghaw aliha direct had def atakhdm"""
+        s = ""
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    s += "{}".format(self.print_symbol)
+                s += "\n"
+        s = s[:-1]
+        return s
+
+    def __repr__(self):
+        """anakhduu repr d def w t executa b eval"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """del an object when we cal del"""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
