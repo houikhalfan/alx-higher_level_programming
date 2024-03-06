5a61f9f4ad22ab7c7bacaa97a2798ef54ffe94f1
houikhalfan <h.ouikhalfan@gmail.com>
2024-03-06 06:08:27 -0800

    advv

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -93,14 +93,16 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+    
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            raise ValueError("N must be at least 4")
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    board = init_board(int(sys.argv[1]))
+    board = init_board(n)
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
         print(sol)
