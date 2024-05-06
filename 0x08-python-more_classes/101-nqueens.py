4c47de45a5c15aea6f3aecca94abe7b6603669a0
houikhalfan <h.ouikhalfan@gmail.com>
2024-05-06 12:20:40 +0100

    ha

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,59 +4,68 @@ N queens backtracking program to print the coordinates of N queens
 on an NxN grid such that they are all in non-attacking positions
 """
 
+import sys
 
-from sys import argv
+def solve_n_queens(n):
+    """Solve the N queens problem and return all solutions"""
+    solutions = []
 
-if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    n = int(argv[1])
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
+    def is_valid(board, row, col):
+        """Check if placing a queen at board[row][col] is valid"""
+        # Check if there's already a queen in the same column
+        for i in range(row):
+            if board[i][col] == 1:
                 return False
-            i += 1
+
+        # Check upper diagonal on the left side
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        # Check upper diagonal on the right side
+        for i, j in zip(range(row, -1, -1), range(col, n)):  # Adjusted range
+            if board[i][j] == 1:
+                return False
+
         return True
 
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
+    def n_queens_helper(board, row):
+        """Helper function for recursive backtracking"""
+        if row == n:
+            queens = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        queens.append([i, j])
+            solutions.append(queens)
+            return
+        for col in range(n):
+            if is_valid(board, row, col):
+                board[row][col] = 1
+                n_queens_helper(board, row + 1)
+                board[row][col] = 0
 
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+    # Initialize empty board
+    board = [[0] * n for _ in range(n)]
 
-    # start the recursive process at x = 0
-    nqueens(0)
+    # Start recursive backtracking
+    n_queens_helper(board, 0)
+
+    return solutions
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solutions = solve_n_queens(n)
+    for solution in solutions:
+        print(solution)
