023bb79bbc2243fe78a87be1974b49ce62bc4839
houikhalfan <h.ouikhalfan@gmail.com>
2024-05-06 15:36:06 -0700

    11

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """file handling"""
 
+
 def read_file(filename=""):
     """reading from a file using with to close automaticly"""
     with open(filename, 'r', encoding='utf-8') as a_file:
